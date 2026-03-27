4d05d7c5aa743fe3e4112c301f744edefa0ad938
sebastiancofrecabanas-bit <sebastiancofrecabanas@gmail.com>
2026-03-26 21:51:30 -0300

    Inicio

---
+++ scofre_css/script.js
@@ -0,0 +1,3 @@
+document.getElementById('dynamicButton').addEventListener('click', function() {
+    document.getElementById('dynamicText').textContent = 'Si este mensaje sale es porque el botón funciono, epico';
+});
