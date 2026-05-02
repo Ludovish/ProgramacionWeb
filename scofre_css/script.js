8f6d2683520136c3ce45614520ecd946a79e9517
Ludovish <233360543+sebastiancofrecabanas-bit@users.noreply.github.com>
2026-05-02 15:49:05 -0400

    Delete scofre_css directory

--- scofre_css/script.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('dynamicButton').addEventListener('click', function() {
-    document.getElementById('dynamicText').textContent = 'Si este mensaje sale es porque el botón funciono, epico';
-});
