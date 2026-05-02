0bb21b82896201c04a5b0622efd5d5f7a97fcd5e
Ludovish <233360543+Ludovish@users.noreply.github.com>
2026-05-02 18:21:43 -0400

    Actualización de app.js y index.html
    Se añadio un tabla para ver la lista de solicitudes.

--- src/presentacion/public/app.js
+++ src/presentacion/public/app.js
@@ -1,4 +1,5 @@
 //Frontend 
+//Primera función para la inserción de datos.
 document.getElementById('formulario').addEventListener('submit', async (e) => { 
     e.preventDefault(); 
     // addEventListener esta atento a cuando se aprete el botón.
@@ -28,4 +29,59 @@ document.getElementById('formulario').addEventListener('submit', async (e) => {
         json.ok ? 'Solicitud enviada correctamente' : json.error;
     //Espera respuesta del backend y muestra un mensaje 
     //dependiendo de si la solicitud fue exitosa o no.
+    if(json.ok) {
+        document.getElementById('formulario').reset();
+    }
+    // Si la solicitud fue exitosa, resetea el formulario para que quede vacío.
+    
+});
+
+//Segunda función para mostrar la lista de
+document.getElementById('btnListar').addEventListener('click', async () => {
+    const contenedor = document.getElementById('listaSolicitudes'); 
+    
+    try {
+        const res = await fetch('/api/solicitudes'); 
+        const solicitudes = await res.json();
+
+        contenedor.innerHTML = '';
+
+        if (solicitudes.length === 0) {
+            contenedor.innerHTML = '<p>No hay solicitudes registradas.</p>';
+            return;
+            // Lista vacia = mensaje para informar.
+        }
+
+        let tabla = `<table border="1" style="width:100%; margin-top:10px; border-collapse: collapse;">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Correo</th>
+                    <th>Mensaje</th>
+                </tr>
+            </thead>
+            <tbody>`;
+        //Se declara una variable "tabla" que contiene el código HTML para crear una tabla con encabezados para ID, Nombre, Correo y Mensaje.
+
+
+        solicitudes.forEach(s => {
+            tabla += `<tr>
+                <td>${s.id}</td>
+                <td>${s.nombre}</td>
+                <td>${s.correo}</td>
+                <td>${s.mensaje}</td>
+            </tr>`;
+        });
+        // Recorre cada solicitud en el array "solicitudes" y agrega una fila a la tabla con los datos de cada solicitud.
+
+
+        tabla += `</tbody></table>`;
+        contenedor.innerHTML = tabla;
+        // Coloca etiquetas de cierra para la tabla para que sea valido.
+
+    } catch (error) {
+        console.error(error);
+        contenedor.innerHTML = '<p style="color:red;">Error al cargar los datos.</p>';
+    }
 });
